8081bb4a12ec0031a039a827c305009d6b404f0d
liuwenhao <924666990@qq.com>
2018-05-26 17:00:39 +0800

    【版本】2.4.2【介绍】里程碑版本，将5大模块分离出来【功能】1、自动完成Activity、Fragment、Dialog中View的绑定 2、MVP的支持 3、企业级日志系统 4、TextUtils、NumberUtils、IoUtils、ImageUtils等工具类 5、ORM数据库关系映射框架 6、多种颜色值代码 7、万能AdapterView适配器 8、ShadeView滑动颜色渐变控件 9、水平Tab滚动条 10、多行单选按钮组 11、字母导航控件 12、可嵌套在ScrollView内部的ListView 13、雷达扫描控件 14、流式布局 15、带清除按钮的文本框 16、高亮背景可游走的TextView 17、自动切换焦点的文本框和文本框组 18、动画框架 19、底部导航栏 20、仪表控件 21、标题栏 22、颜色拾取控件 23、动画框架2 24、侧滑菜单 25、带进度的按钮 26、GIF动画引擎 27、环形变速进度条

---
+++ jackknife-ioc/src/main/java/com/lwh/jackknife/ioc/ViewInjector.java
@@ -0,0 +1,323 @@
+/*
+ *
+ *  * Copyright (C) 2017 The JackKnife Open Source Project
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package com.lwh.jackknife.ioc;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lwh.jackknife.ioc.exception.IllegalViewClassNameException;
+import com.lwh.jackknife.ioc.exception.InjectException;
+import com.lwh.jackknife.ioc.exception.ViewTypeException;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class ViewInjector {
+
+    private final String METHOD_SET_CONTENT_VIEW = "setContentView";
+    private final String METHOD_FIND_VIEW_BY_ID = "findViewById";
+    private final String METHOD_INFLATE = "inflate";
+    private final String METHOD_VALUE = "value";
+    private final String UNDERLINE = "_";
+    private final String R_ID = ".R$id";
+    private final String R_LAYOUT = ".R$layout";
+    private final int A_INDEX = 'A';
+    private final int Z_INDEX = 'Z';
+    private final String VIEW_TYPE_ERROR = "The viewInjected must be an activity or a fragment.";
+    private final String VIEW_CLASS_NAME_ERROR = "Class name is not ends with \'Activity\' or " +
+            "\'Fragment\'.";
+
+    public enum ViewType {
+        UNDECLARED,
+        Activity,
+        Fragment,
+        Dialog
+    }
+
+    private static ViewInjector sInstance;
+
+    private ViewInjector() {
+    }
+
+    private static ViewInjector getInstance() {
+        if (sInstance == null) {
+            synchronized (ViewInjector.class) {
+                if (sInstance == null) {
+                    sInstance = new ViewInjector();
+                }
+            }
+        }
+        return sInstance;
+    }
+
+    public static void inject(SupportV v) {
+        if (v instanceof SupportActivity || v instanceof SupportDialog) {
+            injectLayout(v);
+            injectViews(v);
+            injectEvents(v);
+        } else if (v instanceof SupportFragment) {
+            injectViews(v);
+            injectEvents(v);
+        }
+    }
+
+    protected SupportActivity getSupportActivity(SupportV v) {
+        ViewType viewType = getViewType(v);
+        SupportActivity activity = null;
+        if (viewType == ViewType.Activity) {
+            activity = (SupportActivity) v;
+        } if (viewType == ViewType.Fragment) {
+            activity = ((SupportFragment) v).getFragmentActivity();
+        } else if (viewType == ViewType.Dialog) {
+            activity = ((SupportDialog) v).getDialogActivity();
+        } else if (viewType == ViewType.UNDECLARED) {
+            throw new ViewTypeException(VIEW_TYPE_ERROR);
+        }
+        return activity;
+    }
+
+    protected String generateLayoutName(SupportV v) {
+        ViewType viewType = getViewType(v);
+        if (isViewTypeAllowed(viewType)) {
+            String suffix = viewType.name();
+            StringBuffer sb;
+            String layoutName = v.getClass().getSimpleName();
+            if (!layoutName.endsWith(suffix)) {
+                throw new IllegalViewClassNameException(VIEW_CLASS_NAME_ERROR);
+            } else {
+                String name = layoutName.substring(0, layoutName.length() - suffix.length());
+                sb = new StringBuffer(suffix.toLowerCase(Locale.ENGLISH));
+                for (int i = 0; i < name.length(); i++) {
+                    if (name.charAt(i) >= A_INDEX && name.charAt(i) <= Z_INDEX || i == 0) {
+                        sb.append(UNDERLINE);
+                    }
+                    sb.append(String.valueOf(name.charAt(i)).toLowerCase(Locale.ENGLISH));
+                }
+            }
+            return sb.toString();
+        } else {
+            throw new ViewTypeException(VIEW_TYPE_ERROR);
+        }
+    }
+
+    protected ViewType getViewType(SupportV v) {
+        Class<? extends SupportV> viewClass = v.getClass();
+        if (SupportActivity.class.isAssignableFrom(viewClass)) {
+            return ViewType.Activity;
+        } else if (SupportFragment.class.isAssignableFrom(viewClass)) {
+            return ViewType.Fragment;
+        } else if (SupportDialog.class.isAssignableFrom(viewClass)) {
+            return ViewType.Dialog;
+        } else {
+            return ViewType.UNDECLARED;
+        }
+    }
+
+    protected boolean isViewTypeAllowed(ViewType viewType){
+        return viewType != ViewType.UNDECLARED;
+    }
+
+    public static View injectLayout(SupportV v) {
+        return getInstance()._injectLayout(v);
+    }
+
+    private final View _injectLayout(SupportV v) {
+        View view = null;
+        ViewType viewType = getViewType(v);
+        if (isViewTypeAllowed(viewType)) {
+            String layoutName = generateLayoutName(v);
+            SupportActivity activity = getSupportActivity(v);
+            Class<? extends SupportV> viewClass = v.getClass();
+            String packageName = activity.getPackageName();
+            int layoutId = View.NO_ID;
+            try {
+                Class<?> layoutClass = Class.forName(packageName + R_LAYOUT);
+                Field field = layoutClass.getDeclaredField(layoutName);
+                layoutId = field.getInt(v);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            }
+            ContentView contentView = v.getClass().getAnnotation(ContentView.class);
+            if (contentView != null) {
+                layoutId = contentView.value();
+            }
+            try {
+                if (viewType == ViewType.Activity || viewType == ViewType.Dialog) {
+                    Method method = viewClass.getMethod(METHOD_SET_CONTENT_VIEW, int.class);
+                    method.invoke(activity, layoutId);
+                } else if (viewType == ViewType.Fragment) {
+                }
+                LayoutInflater inflater = LayoutInflater.from((Context) activity);
+                Class<? extends LayoutInflater> inflaterClass = LayoutInflater.class;
+                Method inflateMethod = inflaterClass.getDeclaredMethod(METHOD_INFLATE, int.class,
+                        ViewGroup.class);
+                view = (View) inflateMethod.invoke(inflater, layoutId, null);
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        } else throw new ViewTypeException(VIEW_TYPE_ERROR);
+        return view;
+    }
+
+    public static void injectViews(SupportV v) {
+        getInstance()._injectViews(v);
+    }
+
+    private final void _injectViews(SupportV v) {
+        ViewType viewType = getViewType(v);
+        if (isViewTypeAllowed(viewType)) {
+            Class<?> viewClass = v.getClass();
+            Field[] viewFields = viewClass.getDeclaredFields();
+            SupportActivity activity = getSupportActivity(v);
+            Class<? extends SupportActivity> activityClass = activity.getClass();
+            for (Field field : viewFields) {
+                field.setAccessible(true);
+                Class<?> fieldType = field.getType();
+                if (View.class.isAssignableFrom(fieldType)) {
+                    ViewIgnore viewIgnore = field.getAnnotation(ViewIgnore.class);
+                    if (viewIgnore != null) {
+                        continue;
+                    }
+                    ViewId viewId = field.getAnnotation(ViewId.class);
+                    int id;
+                    try {
+                        if (viewId != null) {
+                            id = viewId.value();
+                        } else {
+                            String packageName = activity.getPackageName();
+                            Class<?> idClass = Class.forName(packageName + R_ID);
+                            Field idField = idClass.getDeclaredField(field.getName());
+                            id = idField.getInt(idField);
+                        }
+                        Method findViewByIdMethod = activityClass.getMethod(METHOD_FIND_VIEW_BY_ID,
+                                int.class);
+                        Object view = findViewByIdMethod.invoke(activity, id);
+                        if (view != null) {
+                            field.set(v, view);
+                        } else {
+                            throw new InjectException(
+                                    fieldType.getName()+" id("+id+") can\'t be injected.");
+                        }
+                    } catch (NoSuchMethodException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    } catch (NoSuchFieldException e) {
+                        e.printStackTrace();
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } else {
+            throw new ViewTypeException(VIEW_TYPE_ERROR);
+        }
+    }
+
+    public static void injectEvents(SupportV v) {
+        getInstance()._injectEvents(v);
+    }
+
+    private final void _injectEvents(SupportV v) {
+        ViewType viewType = getViewType(v);
+        if (isViewTypeAllowed(viewType)) {
+            Class<?> viewClass = v.getClass();
+            Method[] methods = viewClass.getDeclaredMethods();
+            SupportActivity activity = getSupportActivity(v);
+            for (Method method : methods) {
+                Annotation[] annotations = method.getAnnotations();
+                for (Annotation annotation : annotations) {
+                    Class<? extends Annotation> annotationType = annotation.annotationType();
+                    EventBase eventBase = annotationType.getAnnotation(EventBase.class);
+                    if (eventBase == null) {
+                        continue;
+                    }
+                    String listenerSetter = eventBase.listenerSetter();
+                    Class<?> listenerType = eventBase.listenerType();
+                    String callbackMethod = eventBase.callbackMethod();
+                    try {
+                        Method valueMethod = annotationType.getDeclaredMethod(METHOD_VALUE);
+                        int[] viewIds = (int[]) valueMethod.invoke(annotation);
+                        for (int viewId : viewIds) {
+                            View view = activity.findViewById(viewId);
+                            if (view == null) {
+                                continue;
+                            }
+                            Method setListenerMethod = view.getClass().getMethod(listenerSetter,
+                                    listenerType);
+                            HashMap<String, Method> map = new HashMap();
+                            map.put(callbackMethod, method);
+                            EventInvocationHandler handler = new EventInvocationHandler(map, v);
+                            Object proxy = Proxy.newProxyInstance(listenerType.getClassLoader(),
+                                    new Class<?>[]{listenerType}, handler);
+                            setListenerMethod.invoke(view, proxy);
+                        }
+                    } catch (NoSuchMethodException e) {
+                        e.printStackTrace();
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } else {
+            throw new ViewTypeException(VIEW_TYPE_ERROR);
+        }
+    }
+
+    private class EventInvocationHandler implements InvocationHandler {
+
+        private Map<String, Method> mCallbackMethodMap;
+        private SupportV mViewInjected;
+
+        public EventInvocationHandler(HashMap<String, Method> callbackMethodMap, SupportV v) {
+            this.mCallbackMethodMap = callbackMethodMap;
+            this.mViewInjected = v;
+        }
+
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            String name = method.getName();
+            Method callbackMethod = mCallbackMethodMap.get(name);
+            if (callbackMethod != null) {
+                return callbackMethod.invoke(mViewInjected, args);
+            }
+            return method.invoke(proxy, args);
+        }
+    }
+}
---
+++ jackknife-util/src/main/java/com/lwh/jackknife/util/SystemUtils.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2017 The JackKnife Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lwh.jackknife.util;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import java.util.List;
+
+public class SystemUtils {
+
+    private SystemUtils() {
+    }
+
+    public static int getScreenWidth(Context context) {
+        return ScreenUtils.getInstance(context).getScreenWidth();
+    }
+
+    public static int getScreenHeight(Context context) {
+        return ScreenUtils.getInstance(context).getScreenHeight();
+    }
+
+    public static void openKeyboard(EditText editText, Context context) {
+        InputMethodManager imm = (InputMethodManager) context
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(editText, InputMethodManager.RESULT_SHOWN);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,
+                InputMethodManager.HIDE_IMPLICIT_ONLY);
+    }
+
+    public static void closeKeyboard(EditText editText, Context context) {
+        InputMethodManager imm = (InputMethodManager) context
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+    }
+
+    public static boolean isBackground(Context context) {
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> processes = activityManager.getRunningAppProcesses();
+        for (ActivityManager.RunningAppProcessInfo processInfo : processes) {
+            if (processInfo.processName.equals(context.getPackageName())) {
+                return processInfo.importance != ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND;
+            }
+        }
+        return false;
+    }
+}
