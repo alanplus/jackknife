183dee4d014b74740fe906764878cbef7f87c135
liuwenhao <924666990@qq.com>
2018-10-24 23:53:35 +0700

    【版本】2.6.6【介绍】新增LoadingView【功能】1、自动完成Activity、Fragment、Dialog中View的绑定 2、MVP的支持 3、企业级日志系统 4、TextUtils、NumberUtils、IoUtils、ImageUtils等工具类 5、ORM数据库关系映射框架 6、多种颜色值代码 7、万能AdapterView适配器 8、ShadeView滑动颜色渐变控件 9、水平Tab滚动条 10、多行单选按钮组 11、字母导航控件 12、可嵌套在ScrollView内部的ListView 13、雷达扫描控件 14、流式布局 15、带清除按钮的文本框 16、高亮背景可游走的TextView 17、自动切换焦点的文本框和文本框组 18、动画框架 19、底部导航栏 20、仪表控件 21、标题栏 22、颜色拾取控件 23、动画框架2 24、侧滑菜单 25、带进度的按钮 26、GIF动画引擎 27、环形变速进度条 28、跑马灯 29、带翻转效果的ImageView 30、吞没事件的RelativeLayout 31、变色的TextView 32、圆形头像ImageView 33、侧滑菜单2 34、刮刮卡控件 35、幸运转盘 36、下拉刷新和上拉加载 37、基础对话框 38、底部弹出菜单 39.圆形旋转进度条

---
+++ jackknife-orm/src/main/java/com/lwh/jackknife/orm/dao/Dao.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2017 The JackKnife Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lwh.jackknife.orm.dao;
+
+import com.lwh.jackknife.orm.OrmTable;
+import com.lwh.jackknife.orm.builder.QueryBuilder;
+import com.lwh.jackknife.orm.builder.WhereBuilder;
+
+import java.util.List;
+
+public interface Dao<T extends OrmTable> {
+
+    boolean insert(T bean);
+
+    boolean insert(List<T> beans);
+
+    boolean delete(WhereBuilder builder);
+
+    boolean deleteAll();
+
+    boolean update(WhereBuilder builder, T newBean);
+
+    boolean updateAll(T newBean);
+
+    List<T> selectAll();
+
+    List<T> select(QueryBuilder builder);
+
+    T selectOne();
+
+    T selectOne(QueryBuilder builder);
+
+    long selectCount();
+
+    long selectCount(QueryBuilder builder);
+}
---
+++ jackknife-orm/src/main/java/com/lwh/jackknife/orm/dao/DaoFactory.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2017 The JackKnife Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lwh.jackknife.orm.dao;
+
+import com.lwh.jackknife.orm.OrmTable;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class DaoFactory {
+
+    private static Map<Class<? extends OrmTable>,OrmDao> sDaoMap = new ConcurrentHashMap<>();
+
+    public synchronized static <T extends OrmTable> OrmDao<T> getDao(Class<T> beanClass) {
+        if (sDaoMap.containsKey(beanClass)) {
+            return sDaoMap.get(beanClass);
+        } else {
+            OrmDao<T> dao = new OrmDao<>(beanClass);
+            sDaoMap.put(beanClass, dao);
+            return dao;
+        }
+    }
+}
