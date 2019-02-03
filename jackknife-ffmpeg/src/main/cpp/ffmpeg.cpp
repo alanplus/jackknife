f559c97de2bfdfed6b42ed036f055e5574ef961f
liuwenhao <924666990@qq.com>
2019-02-04 03:26:40 +0800

    新增视频像素数据常量定义

--- jackknife-ffmpeg/src/main/cpp/ffmpeg.cpp
+++ jackknife-ffmpeg/src/main/cpp/ffmpeg.cpp
@@ -5,19 +5,3 @@ extern "C"
 {
 #include "libavformat/avformat.h"
 }
-
-
-extern "C"
-JNIEXPORT jstring
-
-JNICALL
-Java_com_yj_ffmpegdemo_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    //注册
-    av_register_all();
-
-
-    return env->NewStringUTF(hello.c_str());
-}
