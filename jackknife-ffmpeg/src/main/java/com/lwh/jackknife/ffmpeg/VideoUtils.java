69fdd99914eb0953f0191f3fddfb6631147009a2
liuwenhao <924666990@qq.com>
2019-02-04 03:25:22 +0800

    新增视频像素数据常量定义

--- jackknife-ffmpeg/src/main/java/com/lwh/jackknife/ffmpeg/VideoUtils.java
+++ jackknife-ffmpeg/src/main/java/com/lwh/jackknife/ffmpeg/VideoUtils.java
@@ -2,6 +2,26 @@ package com.lwh.jackknife.ffmpeg;
 
 public class VideoUtils {
 
+    /**
+     * 视频像素数据YUV420P和YUV420SP。
+     */
+    public static final int PIX_FMT_YUV420 = 0x01;
+
+    /**
+     * 视频像素数据YUV422。
+     */
+    public static final int PIX_FMT_YUV422 = 0x02;
+
+    /**
+     * 视频像素数据YUV444。
+     */
+    public static final int PIX_FMT_YUV444 = 0x03;
+
+    /**
+     * 视频像素数据RGB24。
+     */
+    public static final int PIX_FMT_RGB24 = 0x04;
+
     public native static void reduceVideoQuality(int bitrate);
     public native static void adjustVideoSize(String inputPath, int widthPixels, int heightPixels);
 }
