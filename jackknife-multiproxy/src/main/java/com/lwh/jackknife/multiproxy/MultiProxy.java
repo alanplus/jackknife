fee26f280756c44475cbb77a639d2b0298c9fb94
Alm <924666990@qq.com>
2019-03-29 13:52:50 +0700

    优化MultiProxy

--- jackknife-multiproxy/src/main/java/com/lwh/jackknife/multiproxy/MultiProxy.java
+++ jackknife-multiproxy/src/main/java/com/lwh/jackknife/multiproxy/MultiProxy.java
@@ -1,16 +1,18 @@
 package com.lwh.jackknife.multiproxy;
 
-import com.lwh.jackknife.multiproxy.annotation.Autowired;
 import com.lwh.jackknife.multiproxy.interfaces.DecoratorFactory;
 import com.lwh.jackknife.multiproxy.interfaces.IDifference;
 import com.lwh.jackknife.multiproxy.loader.FactoryProducer;
 
-import java.lang.reflect.Field;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.Element;
 
 public class MultiProxy {
+
+    private static Holder sDecoratorHolder;
 
     private MultiProxy() {
     }
@@ -19,33 +21,42 @@ public class MultiProxy {
         return env.getElementUtils().getPackageOf(element).getQualifiedName().toString();
     }
 
+    public static class Holder {
+        ConcurrentMap<String, IDifference> mDecoratorMap;
+
+        Holder() {
+            mDecoratorMap = new ConcurrentHashMap();
+        }
+
+        public boolean checkDecorator(String name) {
+            return mDecoratorMap.containsKey(name);
+        }
+
+        public void addDecorator(IDifference difference) {
+            String name = difference.getClass().getName();
+            if (!checkDecorator(name)) {
+                mDecoratorMap.put(name, difference);
+            }
+        }
+    }
+
+    public static Holder getHolder() {
+        if (sDecoratorHolder == null) {
+            synchronized (Holder.class) {
+                if (sDecoratorHolder == null) {
+                    sDecoratorHolder = new Holder();
+                }
+            }
+        }
+        return sDecoratorHolder;
+    }
+
     public static <M extends IDifference, D extends IDifference> D
         getDecorator(M module, Class<D> differenceClazz) {
         Class<? extends IDifference> moduleClazz = module.getClass();
-        Field[] fields = moduleClazz.getDeclaredFields();
-        for (Field field : fields) {
-            Autowired autowired = field.getAnnotation(Autowired.class);
-            if (autowired == null) {
-                continue;
-            }
-            try {
-                field.setAccessible(true);
-                Object o = field.get(module);
-                if (o == null) {
-                    synchronized (moduleClazz) {
-                        if (o == null) {
-                            DecoratorFactory factory = FactoryProducer.getFactory(moduleClazz);
-                            o = factory.newDecorator((D) module, differenceClazz);
-                            field.set(module, o);
-                        }
-                        return (D) (o == null ? module : o);
-                    }
-                }
-                return (D) o;
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        }
-        throw new IllegalStateException("注入模块失败");
+        DecoratorFactory factory = FactoryProducer.getFactory(moduleClazz);
+        D decorator = factory.newDecorator((D) module, differenceClazz);
+        getHolder().addDecorator(decorator);
+        return decorator;
     }
 }
