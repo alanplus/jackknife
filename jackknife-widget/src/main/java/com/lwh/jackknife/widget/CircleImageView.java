55a25d3331dbc369b7a6c40ebdbe8451d9da5b29
alm <alm@dafacloud.com>
2020-08-14 09:29:14 +0800

    【JK-11】（增加圆形和圆角矩形ImageView）https://github.com/users/JackWHLiu/projects/3#column-10395957

--- jackknife-widget/src/main/java/com/lwh/jackknife/widget/CircleImageView.java
+++ jackknife-widget/src/main/java/com/lwh/jackknife/widget/CircleImageView.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The JackKnife Open Source Project
+ * Copyright (C) 2020 The JackKnife Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,27 +17,9 @@
 package com.lwh.jackknife.widget;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapShader;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Shader;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.util.AttributeSet;
 
-import androidx.appcompat.widget.AppCompatImageView;
-
-public class CircleImageView extends AppCompatImageView {
-    private Bitmap mBitmap;
-
-    private static final Bitmap.Config BITMAP_CONFIG = Bitmap.Config.ARGB_8888;
-    private static final int COLORDRAWABLE_DIMENSION = 2;
-
-    private Paint mBitmapPaint;
-    private BitmapShader mBitmapShader;
+public class CircleImageView extends RoundRectImageView {
 
     public CircleImageView(Context context) {
         super(context);
@@ -51,64 +33,9 @@ public class CircleImageView extends AppCompatImageView {
         super(context, attrs, defStyleAttr);
     }
 
-    private void setUp() {
-        mBitmapPaint = new Paint();
-        mBitmapShader = new BitmapShader(mBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
-        mBitmapPaint.setAntiAlias(true);
-        mBitmapPaint.setShader(mBitmapShader);
-        invalidate();
-    }
-
     @Override
-    protected void onDraw(Canvas canvas) {
-        int radius = Math.min(getWidth() / 2, getHeight() / 2);
-        canvas.drawCircle(getWidth() / 2, getHeight() / 2, radius, mBitmapPaint);
-    }
-
-    @Override
-    public void setImageResource(int resId) {
-        super.setImageResource(resId);
-        mBitmap = getBitmapFromDrawable(getDrawable());
-        setUp();
-    }
-
-    @Override
-    public void setImageURI(Uri uri) {
-        super.setImageURI(uri);
-        mBitmap = uri != null ? getBitmapFromDrawable(getDrawable()) : null;
-        setUp();
-    }
-
-    @Override
-    public void setImageDrawable(Drawable drawable) {
-        super.setImageDrawable(drawable);
-        mBitmap = getBitmapFromDrawable(drawable);
-        setUp();
-    }
-
-    private Bitmap getBitmapFromDrawable(Drawable drawable) {
-        if (drawable == null) {
-            return null;
-        }
-        if (drawable instanceof BitmapDrawable) {
-            return ((BitmapDrawable) drawable).getBitmap();
-        }
-        try {
-            Bitmap bitmap;
-
-            if (drawable instanceof ColorDrawable) {
-                bitmap = Bitmap.createBitmap(COLORDRAWABLE_DIMENSION, COLORDRAWABLE_DIMENSION, BITMAP_CONFIG);
-            } else {
-                bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), BITMAP_CONFIG);
-            }
-
-            Canvas canvas = new Canvas(bitmap);
-            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-            drawable.draw(canvas);
-            return bitmap;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setCornerRadius(Math.min(getMeasuredWidth() / 2, getMeasuredHeight() / 2));
     }
 }
---
+++ jackknife-widget/src/main/java/com/lwh/jackknife/widget/RoundRectImageView.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2020 The JackKnife Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lwh.jackknife.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+
+import androidx.appcompat.widget.AppCompatImageView;
+
+public class RoundRectImageView extends AppCompatImageView {
+    private Bitmap mBitmap;
+
+    private static final Bitmap.Config BITMAP_CONFIG = Bitmap.Config.ARGB_8888;
+    private static final int COLORDRAWABLE_DIMENSION = 2;
+
+    private Paint mBitmapPaint;
+    private BitmapShader mBitmapShader;
+    private float mCornerRadius;
+
+    public RoundRectImageView(Context context) {
+        this(context, null);
+    }
+
+    public RoundRectImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public RoundRectImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initAttrs(context, attrs);
+    }
+
+    private void initAttrs(Context context, AttributeSet attrs) {
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.RoundRectImageView);
+        mCornerRadius = a.getDimension(R.styleable.RoundRectImageView_rriv_cornerRadius,
+                TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, getResources().getDisplayMetrics()));
+        a.recycle();
+    }
+
+    /**
+     * 设置圆角大小。
+     *
+     * @param cornerRadius 0~width/2, 0~height/2
+     */
+    public void setCornerRadius(float cornerRadius) {
+        this.mCornerRadius = cornerRadius;
+        invalidate();
+    }
+
+    private void setUp() {
+        mBitmapPaint = new Paint();
+        mBitmapShader = new BitmapShader(mBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        mBitmapPaint.setAntiAlias(true);
+        mBitmapPaint.setShader(mBitmapShader);
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawRoundRect(0, 0, getWidth(), getHeight(), mCornerRadius, mCornerRadius, mBitmapPaint);
+    }
+
+    @Override
+    public void setImageResource(int resId) {
+        super.setImageResource(resId);
+        mBitmap = getBitmapFromDrawable(getDrawable());
+        setUp();
+    }
+
+    @Override
+    public void setImageURI(Uri uri) {
+        super.setImageURI(uri);
+        mBitmap = uri != null ? getBitmapFromDrawable(getDrawable()) : null;
+        setUp();
+    }
+
+    @Override
+    public void setImageDrawable(Drawable drawable) {
+        super.setImageDrawable(drawable);
+        mBitmap = getBitmapFromDrawable(drawable);
+        setUp();
+    }
+
+    private Bitmap getBitmapFromDrawable(Drawable drawable) {
+        if (drawable == null) {
+            return null;
+        }
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+        try {
+            Bitmap bitmap;
+
+            if (drawable instanceof ColorDrawable) {
+                bitmap = Bitmap.createBitmap(COLORDRAWABLE_DIMENSION, COLORDRAWABLE_DIMENSION, BITMAP_CONFIG);
+            } else {
+                bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), BITMAP_CONFIG);
+            }
+
+            Canvas canvas = new Canvas(bitmap);
+            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            drawable.draw(canvas);
+            return bitmap;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
