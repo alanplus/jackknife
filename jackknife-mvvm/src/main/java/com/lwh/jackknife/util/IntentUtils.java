87ab381d5c6f35c08587eedd7bd70907982e17c7
Alm <924666990@qq.com>
2020-09-03 00:47:46 +0800

    【JK-22】（优化IntentUtils）https://github.com/users/JackWHLiu/projects/3#column-10672369

--- jackknife-mvvm/src/main/java/com/lwh/jackknife/util/IntentUtils.java
+++ jackknife-mvvm/src/main/java/com/lwh/jackknife/util/IntentUtils.java
@@ -16,9 +16,14 @@
 
 package com.lwh.jackknife.util;
 
+import android.app.Activity;
+import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -28,287 +33,243 @@ public final class IntentUtils {
     private IntentUtils() {
     }
 
-    public static boolean hasExtra(Intent intent, String key) {
+    public static boolean hasExtra(@NonNull Intent intent, @NonNull String key) {
         return intent.hasExtra(key);
     }
 
-    public static boolean getBooleanExtra(Intent intent, String name, boolean defaultValue) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return defaultValue;
+    public static boolean hasExtras(@NonNull Intent intent, @NonNull String[] keys) {
+        int length = keys.length;
+        int count = 0;
+        for (int i = 0; i < length; i++) {
+            if (hasExtra(intent, keys[i])) {
+                count++;
             }
-            return intent.getBooleanExtra(name, defaultValue);
         }
-        return defaultValue;
+        return count == length;
     }
 
-    public static byte getByteExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return 0;
-            }
-            return intent.getByteExtra(name, (byte)0);
-        }
-        return 0;
+    public static void startActivity(@NonNull Class<? extends Activity> activityClazz) {
+        Intent intent = new Intent(GlobalContext.get(), activityClazz);
+        GlobalContext.get().startActivity(intent);
     }
 
-    public static short getShortExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return 0;
-            }
-            return intent.getShortExtra(name, (short) 0);
-        }
-        return 0;
+    public static void startService(@NonNull String action) {
+        Intent intent = new Intent();
+        intent.setAction(action);
+        GlobalContext.get().startService(intent);
     }
 
-    public static char getCharExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return '\n';
-            }
-            return intent.getCharExtra(name, '\n');
-        }
-        return '\n';
+    public static void startService(@NonNull Class<? extends Service> serviceClazz) {
+        Intent intent = new Intent();
+        intent.setClass(GlobalContext.get(), serviceClazz);
+        GlobalContext.get().startService(intent);
     }
 
-    public static int getIntExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return 0;
-            }
-            return intent.getIntExtra(name, 0);
-        }
-        return 0;
+    public static void sendBroadcast(@NonNull String action) {
+        Intent intent = new Intent();
+        intent.setAction(action);
+        GlobalContext.get().sendBroadcast(intent);
     }
 
-    public long getLongExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return 0;
-            }
-            return intent.getLongExtra(name, 0);
-        }
-        return 0;
+    public static void sendBroadcast(@NonNull Class<? extends BroadcastReceiver> broadcastClazz) {
+        Intent intent = new Intent();
+        intent.setClass(GlobalContext.get(), broadcastClazz);
+        GlobalContext.get().sendBroadcast(intent);
     }
 
-    public static float getFloatExtra(Intent intent, String name, float defaultValue) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return defaultValue;
-            }
-            return intent.getFloatExtra(name, defaultValue);
+    public static boolean getBooleanExtra(@NonNull Intent intent, @NonNull String name, boolean defaultValue) {
+        if (!hasExtra(intent, name)) {
+            return defaultValue;
         }
-        return defaultValue;
+        return intent.getBooleanExtra(name, defaultValue);
     }
 
-    public static double getDoubleExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return 0;
-            }
-            return intent.getDoubleExtra(name, 0);
+    public static byte getByteExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return 0;
         }
-        return 0;
+        return intent.getByteExtra(name, (byte) 0);
     }
 
-    public static String getStringExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return null;
-            }
-            return intent.getStringExtra(name);
+    public static short getShortExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return 0;
         }
-        return null;
+        return intent.getShortExtra(name, (short) 0);
     }
 
-    public static CharSequence getCharSequenceExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return null;
-            }
-            return intent.getCharSequenceExtra(name);
+    public static char getCharExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return '\n';
         }
-        return null;
+        return intent.getCharExtra(name, '\n');
     }
 
-    public static <T extends Parcelable> T getParcelableExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return null;
-            }
-            return intent.getParcelableExtra(name);
+    public static int getIntExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return 0;
         }
-        return null;
+        return intent.getIntExtra(name, 0);
     }
 
-    public static Parcelable[] getParcelableArrayExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return null;
-            }
-            return intent.getParcelableArrayExtra(name);
+    public static float getFloatExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return 0;
         }
-        return null;
+        return intent.getFloatExtra(name, 0);
     }
 
-    public <T extends Parcelable> ArrayList<T> getParcelableArrayListExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return null;
-            }
-            return intent.getParcelableArrayListExtra(name);
+    public static double getDoubleExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return 0;
         }
-        return null;
+        return intent.getDoubleExtra(name, 0);
     }
 
-    public static Serializable getSerializableExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return null;
-            }
-            return intent.getSerializableExtra(name);
+    public static String getStringExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return "";
         }
-        return null;
+        return intent.getStringExtra(name);
     }
 
-    public static ArrayList<Integer> getIntegerArrayListExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return null;
-            }
-            return intent.getIntegerArrayListExtra(name);
+    public static CharSequence getCharSequenceExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return "";
         }
-        return null;
+        return intent.getCharSequenceExtra(name);
     }
 
-    public ArrayList<String> getStringArrayListExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return null;
-            }
-            return intent.getStringArrayListExtra(name);
+    public static <T extends Parcelable> T getParcelableExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return null;
         }
-        return null;
+        return intent.getParcelableExtra(name);
     }
 
-    public static ArrayList<CharSequence> getCharSequenceArrayListExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return null;
-            }
-            return intent.getCharSequenceArrayListExtra(name);
+    public static Parcelable[] getParcelableArrayExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return null;
         }
-        return null;
+        return intent.getParcelableArrayExtra(name);
     }
 
-    public static String[] getStringArrayExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return null;
-            }
-            return intent.getStringArrayExtra(name);
+    public static Serializable getSerializableExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return null;
         }
-        return null;
+        return intent.getSerializableExtra(name);
     }
 
-    public static boolean[] getBooleanArrayExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return null;
-            }
-            return intent.getBooleanArrayExtra(name);
+    public static ArrayList<Integer> getIntegerArrayListExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return new ArrayList<>();
         }
-        return null;
+        return intent.getIntegerArrayListExtra(name);
     }
 
-    public static byte[] getByteArrayExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return null;
-            }
-            return intent.getByteArrayExtra(name);
+    public static ArrayList<CharSequence> getCharSequenceArrayListExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return new ArrayList<>();
         }
-        return null;
+        return intent.getCharSequenceArrayListExtra(name);
     }
 
-    public static short[] getShortArrayExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return null;
-            }
-            return intent.getShortArrayExtra(name);
+    public static String[] getStringArrayExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return null;
         }
-        return null;
+        return intent.getStringArrayExtra(name);
     }
 
-    public static char[] getCharArrayExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return null;
-            }
-            return intent.getCharArrayExtra(name);
+    public static boolean[] getBooleanArrayExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return null;
         }
-        return null;
+        return intent.getBooleanArrayExtra(name);
     }
 
-    public static int[] getIntArrayExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return null;
-            }
-            return intent.getIntArrayExtra(name);
+    public static byte[] getByteArrayExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return null;
         }
-        return null;
+        return intent.getByteArrayExtra(name);
     }
 
-    public static long[] getLongArrayExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return null;
-            }
-            return intent.getLongArrayExtra(name);
+    public static short[] getShortArrayExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return null;
         }
-        return null;
+        return intent.getShortArrayExtra(name);
     }
 
-    public static float[] getFloatArrayExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return null;
-            }
-            return intent.getFloatArrayExtra(name);
+    public static char[] getCharArrayExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return null;
         }
-        return null;
+        return intent.getCharArrayExtra(name);
     }
 
-    public static double[] getDoubleArrayExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return null;
-            }
-            return intent.getDoubleArrayExtra(name);
+    public static int[] getIntArrayExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return null;
         }
-        return null;
+        return intent.getIntArrayExtra(name);
     }
 
-    public static CharSequence[] getCharSequenceArrayExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return null;
-            }
-            return intent.getCharSequenceArrayExtra(name);
+    public static long[] getLongArrayExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return null;
         }
-        return null;
+        return intent.getLongArrayExtra(name);
     }
 
-    public static Bundle getBundleExtra(Intent intent, String name) {
-        if (intent != null) {
-            if (!hasExtra(intent, name)) {
-                return null;
-            }
-            return intent.getBundleExtra(name);
+    public static float[] getFloatArrayExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return null;
         }
-        return null;
+        return intent.getFloatArrayExtra(name);
+    }
+
+    public static double[] getDoubleArrayExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return null;
+        }
+        return intent.getDoubleArrayExtra(name);
+    }
+
+    public static CharSequence[] getCharSequenceArrayExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return null;
+        }
+        return intent.getCharSequenceArrayExtra(name);
+    }
+
+    public static Bundle getBundleExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return null;
+        }
+        return intent.getBundleExtra(name);
+    }
+
+    public long getLongExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return 0;
+        }
+        return intent.getLongExtra(name, 0);
+    }
+
+    public <T extends Parcelable> ArrayList<T> getParcelableArrayListExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return null;
+        }
+        return intent.getParcelableArrayListExtra(name);
+    }
+
+    public ArrayList<String> getStringArrayListExtra(@NonNull Intent intent, @NonNull String name) {
+        if (!hasExtra(intent, name)) {
+            return new ArrayList<>();
+        }
+        return intent.getStringArrayListExtra(name);
     }
 }
